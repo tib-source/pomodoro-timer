d114bd8c93f08fa13362c3bcd86c81bcb6571334
tib-source <tibebe1234t@gmail.com>
2021-10-28 23:29:57 +0100

    added function to format timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,14 +2,23 @@ import React, {useState} from 'react'
 import Incrementer from './Incrementer';
 
 function Timer() {
-    const [timer, setTimer] = useState(25); 
-    const [_break, setBreak] = useState(5)
+    const [timer, setTimer] = useState(25);
+    const [seconds, setSeconds] = useState(0);
+    const [_break, setBreak] = useState(5);
+    const [paused, setPaused] = useState(false)
 
+    const formatTimer =()=>{
+     return `${String(timer.toString()).padStart(2,0)} :
+    ${String(seconds.toString()).padStart(2,0)}`
+    }
     return (
-        <div className='timer'>
+        <div className='contianer'>
             <div className="top">
                 <Incrementer state={timer} setState={setTimer} title={"Session"} />
                 <Incrementer state={_break} setState={setBreak} title={"Break"} />
+            </div>
+            <div className="timer">
+                <h1 id="time-left">{formatTimer()}</h1>
             </div>
         </div>
     )
